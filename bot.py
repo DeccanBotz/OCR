5fdfe5fd7c386fbae11ef864ee84fee7f1d14380
Deccan Bots <84326360+DeccanBotz@users.noreply.github.com>
2021-07-22 08:57:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def start(update,context):
     global first
     first=update.message.chat.first_name
     keybord1 = [[InlineKeyboardButton("Owner 👨‍💻", url=f"https://t.me/{OWNER}"),
-                 InlineKeyboardButton("Tutorial 📺", url="https://youtu.be/batik3L32IE")]]
+                 InlineKeyboardButton("Tutorial 📺", url="https://youtu.be/Fu8CgWFTAoM")]]
     reply_markup = InlineKeyboardMarkup(keybord1)
     update.message.reply_text('Hi! '+str(first)+' \n\nWelcome to OCR Bot.\n\nJust send a clear image to me and i will recognize the text in the image and send it as a message!\n\nCheck /help for more...\n\nCreate your Own Bot by Watching Tutorial', reply_markup=reply_markup)
 
@@ -47,7 +47,7 @@ def help(update,context):
     global first
     first=update.message.chat.first_name
     keybord1 = [[InlineKeyboardButton("Owner 👨‍💻", url=f"https://t.me/{OWNER}"),
-                 InlineKeyboardButton("Tutorial 📺", url="https://youtu.be/batik3L32IE")]]
+                 InlineKeyboardButton("Tutorial 📺", url="https://youtu.be/Fu8CgWFTAoM")]]
     reply_markup = InlineKeyboardMarkup(keybord1)
     update.message.reply_text('Hi! '+str(first)+' \n\nFollow these steps...\n➥ First Send me a Clear Image to me \n➥ Select the Language to Extract Selected Language Text in Image \n➥ Extracted Text is Uploaded as Message!', reply_markup=reply_markup)
 
